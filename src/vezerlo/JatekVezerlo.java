c6806243a0d23f0c7dbb267eca6eba9968308c7c
fabuandras <fabuandras@gmail.com>
2025-11-06 16:18:03 +0100

    model javitva

--- src/vezerlo/JatekVezerlo.java
+++ src/vezerlo/JatekVezerlo.java
@@ -1,5 +1,14 @@
 package vezerlo;
 
+import model.JatekModell;
+import nezet.GuiJatekNezet;
+
 public class JatekVezerlo {
+    private GuiJatekNezet nezet;
+    private JatekModell modell;
     
+    public JatekVezerlo(GuiJatekNezet nezet) {
+        this.nezet = nezet;
+        this.modell = new JatekModell();
+    }
 }
